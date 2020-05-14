35d618e572732df96ff7383592b261bc4043ac55
unknown <baraiyavivek04@gmail.com>
2020-05-14 14:47:20 +0530

    Improved Version

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-fetch("http://puzzle.mead.io/puzzle").then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     });
+// });
 
 
 // fetch("http://localhost:3000/weather?address=Diu India").then(res => {
@@ -19,6 +19,8 @@ fetch("http://puzzle.mead.io/puzzle").then((response) => {
 
 const locationText = document.querySelector("input");
 const weatherForm = document.querySelector("form");
+const weatherData = document.querySelector("#location-name");
+
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const searchAddress = locationText.value;
@@ -27,8 +29,10 @@ weatherForm.addEventListener("submit", (e) => {
         res.json().then(data => {
             if (data.error) {
                 console.log(data.error);
+                weatherData.innerHTML = data.error;
             } else {
                 console.log(data.Longitude,data.Latitude,data.Name);
+                weatherData.innerHTML = "Londitude is " + data.Longitude + " and latitude is " + data.Latitude + " for " + data.Name;
             }
         });
     });
